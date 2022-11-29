460f8515daba3e68074a69c1df2b2fd6aa19a655
Desman012 <107124396+Desman012@users.noreply.github.com>
2022-11-29 10:50:48 +0700

    update tugas membuat function selesai

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-//import library readline
-const func = require('./function.js');
+//memanggil local module 'function.js' dengan metode 'require' dan menamai-nya 'ques'
+const ques = require('./function.js');
 
 //membuat fungsi main asynchronous
 const main = async () => {
-    //memanggil fungsi question, dan membuat pertanyaan
+    //memanggil fungsi question, dan membuat pertanyaan disimpan di variable 'name'
     const name = await ques.questionss('what is your name? ')
-    //memanggil 
+    //memanggil fungsi question, dan membuat pertanyaan disimpan di variable 'email'
     const email = await ques.questionss('what is your email? ')
-    //memanggil fungsi qu
+    //memanggil fungsi question, dan membuat pertanyaan disimpan di variable 'tlp'
     const tlp = await ques.questionss('what is your number phone? ')
 
-        //memanggil fungsi untuk menyimpan data ke json
+        //memanggil fungsi 'savedata' untuk menyimpan data ke json
         ques.savedata(name,email,tlp)
 }
 
